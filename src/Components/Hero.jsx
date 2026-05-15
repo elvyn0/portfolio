0d629be3ff3b0f4531c62c08abda41a6d5821d8a
Elvyn <122730895+elvyn0@users.noreply.github.com>
2026-05-16 02:47:24 +0530

    upload updated resume

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -48,15 +48,15 @@ function Hero() {
           {/* CTA */}
           <div className="flex items-center gap-3">
             {/* View */}
-            <a href="/Anoop_S_Software_Developer_Resume.pdf" target="_blank" rel="noopener noreferrer">
+            <a href="/Anoop_Software_Developer_Resume.pdf" target="_blank" rel="noopener noreferrer">
               <button className="bg-black text-white py-2 px-5 rounded-full font-semibold hover:bg-gray-800 transition">
                 Resume
               </button>
             </a>
             {/* Download */}
             <a
-              href="/Anoop_S_Software_Developer_Resume.pdf"
-              download="Anoop_S_Software_Developer_Resume.pdf"
+              href="/Anoop_Software_Developer_Resume.pdf"
+              download="Anoop_Software_Developer_Resume.pdf"
               className="cursor-pointer hover:bg-gray-100 p-1 rounded-full transition-all"
             >
               <Download />
